c2b19915bc3bc7615177cde3db016f339a0aa546
AvDjah <arvindmee25@gmail.com>
2023-07-13 17:19:31 +0530

    Added Banner Style

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,8 +2,8 @@
 
 const Banner = () => {
     return <div className="banner" >
-        <div><h1>Hello, Puneet Dhiman</h1></div>
-        <div><h3 style={{ fontWeight: 300 }} >Following is your organization’s performance summary</h3></div>
+        <div className="banner-text" ><h1>Hello, Puneet Dhiman</h1></div>
+        <div className="banner-text" ><h3 style={{ fontWeight: 300 }} >Following is your organization’s performance summary</h3></div>
     </div>
 }
 
